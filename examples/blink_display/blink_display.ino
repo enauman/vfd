4ce8ca3982f0fe15147204b33888811a55432cc9
Erik Nauman <enauman@hewittschool.org>
2020-10-03 22:27:14 -0400

    Update blink_display.ino

--- examples/blink_display/blink_display.ino
+++ examples/blink_display/blink_display.ino
@@ -1,8 +1,8 @@
 #include <VFD.h>
-const int NUM_SEG_PINS = 9;
-const int NUM_GRID_PINS = 5;
-const int segments[] = {8,7,6,5,4,3,2, A1, A0};
-const int grids[] = {13, 12, 11, 10, 9};
+const byte NUM_SEG_PINS = 9;
+const byte NUM_GRID_PINS = 5;
+const byte segments[] = {8,7,6,5,4,3,2, A1, A0};
+const byte grids[] = {13, 12, 11, 10, 9};
 
 VFD disp(NUM_SEG_PINS, segments, NUM_GRID_PINS, grids);
 
